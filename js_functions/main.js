034f0c5592edb83f428d791561451ca68dcb1ced
Catherine Chan <catherini168@gmail.com>
2019-06-26 22:51:34 -0700

    JS | Functions - Catherine Lim

---
+++ js_functions/main.js
@@ -0,0 +1,15 @@
+function messageAlert() {
+  alert('Hello!!');
+}
+
+
+function add(num1 , num2) {
+  var result = num1 + num2;
+  console.log(result);
+}
+
+function addWithReturn(num3,num4){
+  var result2 = num3 + num4;
+  return result2;
+}
+addWithReturnResult = addWithReturn(22,33);
