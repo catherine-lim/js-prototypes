97ac624573ad45d4673e8e1f0a7e4fd032ac57cb
Catherine Chan <catherini168@gmail.com>
2019-06-30 13:33:25 -0700

    JS | DOM Creation - Catherine Lim

---
+++ js_dom_creation/main.js
@@ -0,0 +1,50 @@
+$(document).ready(initializeApp)
+
+function initializeApp(){
+  console.log("Initializing App...");
+  var dynamicCol = $('.container .col:nth-child(2)');
+  dynamicCol.append('<h1>Dynamic Content</h1>');
+  var firstCard = createCard('club', 'ten');
+  dynamicCol.append(firstCard); // or $(dynamicCol).append(firstCard);
+  var firstRandomCard = createRandomCard();
+  dynamicCol.append(firstRandomCard);
+  addRandomCardToDom();
+  var addCardButton = $('<button>', {text: 'Add Card to DOM',click: addRandomCardToDom});
+  dynamicCol.prepend(addCardButton);
+}
+
+function createCard(suit,rank){
+  var card = $('<div>', {
+    class: 'card ' + suit + ' ' + rank,
+    click: toggleCardBack
+  });
+
+  return card;
+
+}
+
+function createRandomCard(){
+var suitArray = ['heart','club','diamond','spade'];
+var rankArray = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king'];
+var suitIndex = Math.floor(Math.random() * suitArray.length);
+var rankIndex = Math.floor(Math.random() * rankArray.length);
+
+  var randomSuit = suitArray[suitIndex];
+  var randomRank = rankArray[rankIndex];
+  var randomCard = createCard(randomSuit,randomRank);
+  return randomCard;
+}
+
+function addRandomCardToDom(){
+  var randomCard = createRandomCard()
+  $('.container >.col:nth-child(2)').append(randomCard);
+}
+
+
+function toggleCardBack(){
+var clickedCard = $(this);
+  clickedCard.toggleClass('back');
+
+}
+
+// $("<div/>").attr('id','new').appendTo('body');
