4184fb1dc558044d7e1676f2c65873541a4285bc
Catherine Chan <catherini168@gmail.com>
2019-07-14 13:43:11 -0700

    bankaccount

--- js_oop/exercises/bankaccount.js
+++ js_oop/exercises/bankaccount.js
@@ -2,17 +2,27 @@
 
 class Account{
 	constructor(){
+	this.moneyInAccount = 0;
 		//store the amount of money in the account
 	}
-	add(  ){
+	add(amount){
 		//add money to the amount stored in the account
 		//takes in an amount
 		//checks if it is actually a number greater than 0
 			//if not, return false
 		//adds it to the existing amount
 		//returns the new amount in the account
+
+if (amount > 0 ){
+	this.moneyInAccount = this.moneyInAccount + amount;
+	return this.moneyInAccount;
+} else {
+	return false;
+
+}
+
 	}
-	remove( amount ){
+	remove(amount){
 		//removes funds from an account
 		//checks if amount is actually a number greater than 0
 			//if not, return false
@@ -20,8 +30,20 @@ class Account{
 		//if more, only withdraw the amount in the account, not more
 		//if less, withdraw the amount specified
 		//return the amount actually withdrawn
+		if (amount > 0) {
+		} else {
+			return false;
+		}
+		 					if  (amount > this.moneyInAccount) {
+			 						amount -= this.moneyInAccount ;
+									return this.moneyInAccount;
+							} if (amount < this.moneyInAccount) {
+										this.moneyInAccount -= amount ;
+										return amount;
+		}
 	}
-	getAmount(){
+	getAmount( ){
 		//returns the amount in the account
+		return this.moneyInAccount;
+		}
 	}
-}
